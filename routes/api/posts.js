09ccc382bf303981375f4fe7c5bb5c8a85720a3d
Wilian Revejes <wilianrevejes@hotmail.com>
2019-02-13 17:08:06 -0200

    initialConfiguration/Express/Route/Mongo

---
+++ routes/api/posts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// @route GET api/posts/test
+// @desc Tests posts route
+// @acess Public
+
+router.get('/test', (req, res) =>
+  res.json({
+    msg: 'Posts Works'
+  })
+);
+
+module.exports = router;
