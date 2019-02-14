f8422fe6175f2652a5efc5d0063be0dc7d956641
Wilian Revejes <wilianrevejes@hotmail.com>
2019-02-14 14:03:49 -0200

    createLogin/Token

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 //mongo
 module.exports = {
   mongoURI:
-    'mongodb://will:1234@cluster0-shard-00-00-j4tgq.mongodb.net:27017,cluster0-shard-00-01-j4tgq.mongodb.net:27017,cluster0-shard-00-02-j4tgq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+    'mongodb://will:1234@cluster0-shard-00-00-j4tgq.mongodb.net:27017,cluster0-shard-00-01-j4tgq.mongodb.net:27017,cluster0-shard-00-02-j4tgq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  secretOrKey: 'secret'
 };
