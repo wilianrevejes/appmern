765a9428d0a2ad0a96975fe122f468d415b4dcbf
Wilian Revejes <wilianrevejes@hotmail.com>
2019-02-22 16:24:33 -0300

    PostCreateRoute

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load Validation
+const validatePostInput = require('../../validation/post');
+
+// Load Model
+const Post = require('../../models/Post');
 
 // @route GET api/posts/test
 // @desc Tests posts route
@@ -10,5 +18,26 @@ router.get('/test', (req, res) =>
     msg: 'Posts Works'
   })
 );
+
+// @route POST api/posts
+// @desc Create post
+// @acess Private
+router.post('/', passport.authenticate('jwt', { session: false }),  (req,res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+  // Check Validation
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id
+  });
+  
+  newPost.save().then(post => res.json(post));
+});
+
 
 module.exports = router;
