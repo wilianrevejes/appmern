ef4d1ed90df583e30a6c4e57c59d759227361678
Wilian Revejes <wilianrevejes@hotmail.com>
2019-02-25 20:30:58 -0300

    TestingRegistrationFromWithoutRedux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Register extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       name: '',
@@ -16,9 +17,9 @@ class Register extends Component {
   }
 
   onChange(e) {
-    this.setState({[e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   }
-  
+
   onSubmit(e) {
     e.preventDefault();
 
@@ -28,6 +29,10 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     }
+
+    axios.post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data));
 
     console.log(newUser);
   }
@@ -42,48 +47,48 @@ class Register extends Component {
               <p className="lead text-center">Create your DevConnector account</p>
               <form onSubmit={this.onSubmit}>
                 <div className="form-group">
-                  <input 
-                    type="text" 
-                    className="form-control form-control-lg" 
-                    placeholder="Name" 
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Name"
                     name="name"
                     value={this.state.name}
                     onChange={this.onChange}
-                   />
+                  />
                 </div>
                 <div className="form-group">
-                  <input 
-                    type="email" 
-                    className="form-control form-control-lg" 
-                    placeholder="Email Address" 
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
                     name="email"
                     value={this.state.email}
                     onChange={this.onChange}
-                    />
+                  />
                   <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
                 </div>
                 <div className="form-group">
-                  <input 
-                    type="password" 
-                    className="form-control form-control-lg" 
-                    placeholder="Password" 
-                    name="password" 
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
                     value={this.state.password}
                     onChange={this.onChange}
-                    />
+                  />
                 </div>
                 <div className="form-group">
-                  <input 
-                    type="password" 
-                    className="form-control form-control-lg" 
-                    placeholder="Confirm Password" 
-                    name="password2" 
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Confirm Password"
+                    name="password2"
                     value={this.state.password2}
                     onChange={this.onChange}
-                    />
+                  />
                 </div>
-                <input 
-                  type="submit" 
+                <input
+                  type="submit"
                   className="btn btn-info btn-block mt-4" />
               </form>
             </div>
